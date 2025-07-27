037b7d32c5367e7024d1a9e4fd8c5260dab6189a
Senri-t <izoyata@gmail.com>
2025-07-27 22:45:32 +0900

    2-8

---
+++ 2-8/Task2_8.java
@@ -0,0 +1,27 @@
+public class Task2_8 {
+    public static void main(String[] args) {
+        int plus = 5 + 16;
+
+        int minus = 27 - 7;
+
+    // 10 と　2 を乗算した値を代入する、変数 multiply を作成
+    int multiply = 10 * 2;
+
+    int remi = 20 % 6;
+
+    // 文字列型の変数 town に "中央"、line に "線" を代入
+    String town = "中央";
+    String line = "線";
+
+
+    String train = town + line;
+
+    
+    System.out.println(plus);
+    System.out.println(minus);
+    System.out.println(multiply);
+    System.out.println(remi);
+    System.out.println(train);
+
+    }
+}
