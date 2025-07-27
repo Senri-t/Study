40dfa9ce37365e45e5a64a9ed9de20431c7bdb54
Senri-t <izoyata@gmail.com>
2025-07-27 17:36:17 +0900

    2-6

---
+++ 2-6/Task2_6.java
@@ -0,0 +1,18 @@
+public class Task2_6 {
+  public static void main(String[] args) {
+    int i = 50;
+    String hogeString ="テスト";
+    //データ型変数 hogeDouble に値 0.8 を代入
+    double hogeDouble = 0.8;
+    int hogeInt = 100;
+    hogeString = "良くできました！";
+
+
+    System.out.println(hogeInt);
+
+    //変数 hogeStringを表示する
+    System.out.println(hogeString);
+
+    System.out.println(hogeDouble);
+  }
+}
