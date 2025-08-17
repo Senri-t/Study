cef96d44362f921ceaa5289b0fb5ac45a97d10d0
Senri-t <izoyata@gmail.com>
2025-08-17 20:36:38 +0900

    Update Task2_11.java

--- 2-11/Task2_11.java
+++ 2-11/Task2_11.java
@@ -24,11 +24,11 @@ public class Task2_11 {
 
    int a = 10;
    int b = 3;
-   System.out.println("和：" a + b);
-   System.out.println("差：" a - b);
-   System.out.println("積：" a * b);
-   System.out.println("商：" a / b);
-   System.out.println("余り：" a % b);
+   System.out.println("和：" + (a + b));
+   System.out.println("差：" + (a - b));
+   System.out.println("積：" + (a * b));
+   System.out.println("商：" + (a / b));
+   System.out.println("余り：" + (a % b));
 
 
 
@@ -36,12 +36,13 @@ public class Task2_11 {
     * 問題②
     * int型の配列を宣言し、1から20までの偶数を格納して、ループを使用してそれらの整数を順番に表示して下さい。
     */
-   int[] c= new int[10]
+   int[] c= new int[10];
    for(int i=0; i < c.length; i++){
     c[i] = (i + 1) * 2;
    }
    for (int i = 0; i < c.length; i++) {
-            System.out.print(c[i] + "|");
+            System.out.println(c[i]);
+   }
 
      
      /*
@@ -58,10 +59,11 @@ public class Task2_11 {
 
     for (int i = 0; i < numbers.length; i++) {
     //問題③ 奇数かどうかの判定をするために、for文で取得したnumberを2で割り、余りが0にならないような条件文(if文)を記載して下さい。
-        if(){
+        if(numbers[i] % 2 != 0){
         add += numbers[i];
         }
     }
     return add;
     }
+
 }
