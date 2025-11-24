bc81951a46f44f54b386e45d50c39de90d8258c0
Senri-t <izoyata@gmail.com>
2025-11-24 21:35:59 +0900

    Create Book.java

---
+++ 4ｰ22/src/main/java/com/example4_22/book/entity/Book.java
@@ -0,0 +1,26 @@
+package com.example4_22.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 50)
+    private String author;
+}
